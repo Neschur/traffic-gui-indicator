34f8d0fdee027037cdf10ae6b93a98e8acfe6ab7
Siarhei Hanchuk <siarheihanchuk@gmail.com>
2012-01-22 13:20:52 +0300

    v1.1.0

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
-#include "stdlib.h"
+#include <stdlib.h>
 #include <gtk/gtk.h>
-#include "string.h"
+#include <string.h>
+#include <getopt.h>
 
 GtkWidget *window;
-gboolean visibl=FALSE;
+gboolean visible=TRUE;
 char *interface;
 
 char *GetTraffState()
@@ -17,34 +18,44 @@ char *GetTraffState()
     strcat(pn," |grep bytes");
     fr=popen(pn,"r");
     free(pn);
-    if(fr==NULL){strcpy(res,"Error");return NULL;}
-    char tmp[512];
-    fgets(tmp,sizeof(tmp),fr);
-    printf(tmp);
+    char buf1[512];
+    char buf2[512];
+    if(fr==NULL)
+        return NULL;
+    fgets(buf1,sizeof(buf1),fr);
+    fgets(buf2,sizeof(buf2),fr);
     pclose(fr);
-    if(tmp[0]!=' '){strcpy(res,"No connetction");return res;}
-
+    if(buf1[0]!=' ')
+        return NULL;
     char *str1=(char *)malloc(sizeof(char)*10);
     char *str2=(char *)malloc(sizeof(char)*10);
-    memset(str1,0,10);memset(str2,0,10);
     int i=0;
-    for(i=0;tmp[i]!='(';i++);
-
+    for(i=0; buf1[i]!='('; i++)
+        if(buf1[i]==0)
+            return NULL;
     i++;
     int j=0;
-    while(tmp[i]!=')'){
-        str1[j]=tmp[i];
-        j++;i++;
+    while(buf1[i]!=')'){
+        if(buf1[i]==0)
+            return NULL;
+        str1[j]=buf1[i];
+        j++;
+        i++;
     }
-    int y=0;
-    for(y=i+1;tmp[y]!='(';y++);
-    y++;
+    str1[j]=0;
+    for(i=0; buf2[i]!='('; i++);
+        if(buf2[i]==0)
+            return NULL;
+    i++;
     j=0;
-    while(tmp[y]!=')'){
-        str2[j]=tmp[y];
-        j++;y++;
+    while(buf2[i]!=0){
+        if(buf2[i]==0)
+            return NULL;
+        str2[j]=buf2[i];
+        j++;
+        i++;
     }
-
+    str2[j-2]=0;
     strcpy(res,"In:");
     strcat(res,str1);
     strcat(res," ");
@@ -58,7 +69,6 @@ char *GetTraffState()
 gboolean refTraffState(gpointer data)
 {
     GtkWidget *label = GTK_WIDGET(data);
-    static int counter=1;
     char *str = GetTraffState();
     if(str==NULL){
         str=malloc(sizeof(char)*10);
@@ -66,7 +76,6 @@ gboolean refTraffState(gpointer data)
     }
     gtk_label_set_text(GTK_LABEL(label),str);
     free(str);
-    counter++;
     return TRUE;
 }
 
@@ -75,7 +84,7 @@ GdkPixbuf *create_pixbuf(const gchar * filename)
     GdkPixbuf *pixbuf;
     GError *error = NULL;
     pixbuf = gdk_pixbuf_new_from_file(filename, &error);
-    if(!pixbuf) {
+    if(!pixbuf){
         fprintf(stderr, "%s\n", error->message);
         g_error_free(error);
     }
@@ -84,43 +93,35 @@ GdkPixbuf *create_pixbuf(const gchar * filename)
 
 void iconClick(GtkWidget *widget)
 {
-    if(visibl){
-        gtk_widget_hide(window);
-        visibl=FALSE;
+    if(visible){
+        gtk_widget_hide_all(window);
+        visible=FALSE;
     }else{
-        gtk_widget_show(window);
-        visibl=TRUE;
+        gtk_widget_show_all(window);
+        visible=TRUE;
     }
 }
 
 void gui( int argc, char *argv[])
 {
     GtkWidget *frame;
-    GtkWidget* label;
-
+    GtkWidget *label;
     gtk_init(&argc, &argv);
-
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "traffic");
     gtk_window_set_default_size(GTK_WINDOW(window), 100, 20);
+    gtk_window_set_resizable(GTK_WINDOW(window),FALSE);
     gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
     gtk_window_set_icon(GTK_WINDOW(window), create_pixbuf("/usr/share/icons/hicolor/64x64/apps/traffic-gui-indicator.png"));
-
     frame = gtk_fixed_new();
     gtk_container_add(GTK_CONTAINER(window), frame);
-
     label=gtk_label_new(NULL);
     gtk_fixed_put(GTK_FIXED(frame), label, 0,0);
-
     refTraffState(label);
-
-    gtk_widget_show_all(window);
-    if(visibl){
-        gtk_widget_hide(window);
-        }
-
+    if(visible)
+        gtk_widget_show_all(window);
     g_timeout_add(1000,refTraffState,label);
-
+    g_signal_connect_swapped(G_OBJECT(window), "delete-event", G_CALLBACK(gtk_widget_hide_on_delete), window);
     g_signal_connect_swapped(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
     GtkStatusIcon *icon=gtk_status_icon_new_from_file("/usr/share/icons/hicolor/64x64/apps/traffic-gui-indicator.png");
     gtk_status_icon_set_visible(icon,TRUE);
@@ -128,40 +129,42 @@ void gui( int argc, char *argv[])
     gtk_main();
 }
 
-int main( int argc, char *argv[])
+const struct option long_options[] = {
+		{"version",no_argument,NULL,'v'},
+		{"interface",required_argument,NULL,'i'},
+		{"help",no_argument,NULL,'h'},
+		{"hidewin",no_argument,NULL,'w'},
+		{NULL,0,NULL,0}
+	};
+
+int main(int argc, char *argv[])
 {
-    gboolean o=FALSE;
     interface=(char *)malloc(sizeof(char)*16);
     strcpy(interface,"eth0");
-    if(argc>1)
-    {
-        int i;
-        for(i=0;i<argc;i++){
-            if((strcmp(argv[i],"--auto")==0)||(strcmp(argv[i],"-a")==0)){
-                visibl=TRUE;
-                o=TRUE;
-            }
-            if((strcmp(argv[i],"--interface")==0)||(strcmp(argv[i],"-i")==0)){
-                strcpy(interface,argv[i+1]);
-                o=TRUE;
-            }
-            if((strcmp(argv[i],"--version")==0)||(strcmp(argv[i],"-v")==0)){
-                printf("traffic gui indicator 1.0.1\n");
-                return 1;
-            }
-            if((strcmp(argv[i],"--help")==0)||(strcmp(argv[i],"-h")==0)){
-                printf("simple gui traffic indicator\n"
-                        "--version(-v) - Print version\n"
-                        "--help(-h) - Print this help\n"
-                        "--auto(-a) - for autorun\n"
-                        "--interface(-i) - interface(eth0, wlan0, ppp0, etc)"
-                    );
-                return 1;
-            }
-        }
-        if(!o){
-            printf("unknown\n");
-            return -1;
+    char c;
+    int option_index;
+    while ((c = getopt_long (argc, argv, "wi", long_options, &option_index))!=-1){
+        switch(c){
+        case 'h':
+            printf("simple gui traffic indicator\n"
+                    "--help - Print this help\n"
+                    "--version - Print version\n"
+                    "--hidewin(-w) - start with hided window\n"
+                    "--interface(-i) - interface(eth0, wlan0, ppp0, etc)\n"
+                );
+            return 0;
+        case 'v':
+            printf("simple gui traffic indicator 1.1.0\n");
+            return 0;
+        case 'i':
+            strcpy(interface,optarg);
+            break;
+        case 'w':
+            visible=FALSE   ;
+            break;
+        default:
+            fprintf (stderr, ("Try '%s --help' for more information.\n"), "fb2rn");
+            return 0;
         }
     }
     gui(argc,argv);
--- readme.txt
+++ readme.txt
@@ -1,6 +1,18 @@
 traffic-gui-indicator
 
+simple gui traffic indicator
+shows on small window the traffic spent per session
+
+License:GPLv3+
+
 ChangeLog:
 1.0.1
- добавлена опция --interface
- обрабатыевает все аргументы коммандной строки, а не только первый
+ added --interface
+ handles all the command-line arguments, not only the first
+
+1.1.0
+ Fixed to be compatible with the new ifconfig
+ The command line arguments are processed through gnuopt.h
+ When you close the window, the application hides in the tray
+ Not resizeble
+
